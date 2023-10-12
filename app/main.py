da6410569b715bb59a4a2745586bf966f43d8225
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-10-12 21:03:24 +0300

    Update main.py
    commit py-person-class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
 
+def __init__(self, name, age):
+    self.name = name
+    self.age = age
+    self.wife = None
+    self.husband = None
+    Person.people[name] = self
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for people_dict in people:
+        name = person_dict["name"]
+        age = person_dict["age"]
+        person = Person(name, age)
+        if "wife" in person_dict and person_dict["husband"]:
+            person.husband = Person.people.get(person_dict["husband"])
+            person.husband.wife = person
+        person_list.append(person)
+    return person_list
