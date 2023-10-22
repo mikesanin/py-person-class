b84fe41b0be74c4436a69eb792f56821dc3677eb
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-10-22 14:50:37 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ from typing import List, Optional
 class Person:
     people: dict[str, "Person"] = {}
 
-    def __init__(self, name: str, age: int, spouse: Optional["Person"] = None):
-    -> None:
+    def __init__(self, name: str, age: int, spouse: Optional["Person"] = None) -> None:
         self.name: str = name
         self.age: int = age
         self.spouse: Optional["Person"] = spouse
@@ -18,8 +17,7 @@ def create_person_list(people: List[dict]) -> List[Person]:
     for person_data in people:
         name: str = person_data["name"]
         age: int = person_data["age"]
-        spouse_name: str = person_data.get("wife")
-        or person_data.get("husband")
+        spouse_name: str = person_data.get("wife") or person_data.get("husband")
 
         spouse: Optional[Person] = Person.people.get(spouse_name)
 
