ba0be166c4ee361532ffcc6071173d703a8c0429
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2024-02-11 21:25:47 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None
         Person.people[name] = self
 
 
