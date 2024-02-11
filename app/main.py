1227fd48e5b6e224d561c665ae88521a570aab2f
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2024-02-11 20:44:19 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
-from models import Actor
-from managers import ActorManager
+class Person:
+    people = {}
 
-if __name__ == "__main__":
-    Actor.objects = ActorManager()
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
-    Actor.objects.create(1, "Emma", "Watson")
-    Actor.objects.create(2, "Daniel", "Radclife")
-    print(Actor.objects.all())
+def create_person_list(people: list) -> list:
+    for person in people:
+        if person["name"] not in Person.people:
+            Person(person["name"], person["age"])
+    
+    for person in people:
+        if "wife" in person and person["wife"]:
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+        elif "husband" in person and person["husband"]:
+            Person.people[person["name"]].husband = Person.people[person["husband"]]
+    
+    return list(Person.people.values())
 
-    Actor.objects.update(2, "Daniel", "Radcliffe")
-    print(Actor.objects.all())
+people = [
+    {"name": "Ross", "age": 30, "wife": "Rachel"},
+    {"name": "Joey", "age": 29, "wife": None},
+    {"name": "Rachel", "age": 28, "husband": "Ross"}
+]
 
-    Actor.objects.delete(1)
-    print(Actor.objects.all())
+person_list = create_person_list(people)
