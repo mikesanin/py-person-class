6bd5ac7479692ffeba0bea00f754e8c7c643173c
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2024-02-11 20:30:23 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
-    pass
-
+    people = {}
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person in people:
+        if person['name'] not in Person.people:
+            Person(person['name'], person['age'])
+    
+    for person in people:
+        if 'wife' in person and person['wife']:
+            Person.people[person['name']].wife = Person.people[person['wife']]
+        elif 'husband' in person and person['husband']:
+            Person.people[person['name']].husband = Person.people[person['husband']]
+    
+    return list(Person.people.values())
+
+people = [
+    {"name": "Ross", "age": 30, "wife": "Rachel"},
+    {"name": "Joey", "age": 29, "wife": None},
+    {"name": "Rachel", "age": 28, "husband": "Ross"}
+]
+
+person_list = create_person_list(people)
